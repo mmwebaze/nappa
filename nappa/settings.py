a1da74fa661ca6dabc3b8b2a6fa1da690844d154
Michael Mwebaze <michael.mwebaze@gmail.com>
2015-05-30 08:29:06 -0400

    change postgres password

--- nappa/settings.py
+++ nappa/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'nappa_db',
 		'USER': 'postgres',
-        'PASSWORD': 'dhis2',
+        'PASSWORD': '4g3t1t',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
